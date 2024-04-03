1c6a1c239f1bf7c1f57c1076b63990d555010ecc
Hampus Farner <hampusfarner@outlook.com>
2024-04-03 13:16:13 +0200

    positionToInt function, translates each board position to a number

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-const knightMoves = (function knightMoves(start, end) {})();
+function positionToInt(arr) {
+  return arr[0] + 8 * arr[1];
+}
+const test = [1, 4];
+console.log(positionToInt(test));
+
+// const knightMoves = (function knightMoves(start, end) {
+
+// })();
